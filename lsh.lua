8f4037260fe9590f5e1c15d44eef1aecbcbbfe61
lxpanel <96754598+lxpanel@users.noreply.github.com>
2022-06-30 10:52:31 -0400

    created "pwr" command
    made battery level and state command

--- lsh.lua
+++ lsh.lua
@@ -14,7 +14,19 @@ end
 function help ()
   dofile('help')
 end
-print("lsh -  type \"help\" for a list of commands")
+
+function battery ()
+  percent = io.popen('cat /sys/class/power_supply/BAT1/capacity'):read('*l')
+  print(percent)
+  state = io.popen('cat /sys/class/power_supply/BAT1/status'):read('*l')
+  print(state)
+end
+--function subshell ()
+	--sh = io.popen('sh'):read('*l')
+    --print(sh)
+--end
+
+print("lsh -  type \"help\" for a list of commands press Ctrl+C to exit")
 while true do
   a = io.read()
   if a == "hostname" then host()
@@ -27,5 +39,8 @@ while true do
   end
 
   if a == "help" then help()
+  end
+  
+  if a == "pwr" then battery()
   end
 end
